8100e3b5022eca23107d2039a7cf553fff38ac1f
Andres <andlo2007@hotmail.com>
2016-01-04 17:11:42 -0500

    Primera vez

---
+++ script.sql
@@ -0,0 +1,22 @@
+"DROP TABLE IF EXISTS Usuario; " +
+
+"CREATE TABLE usuario ("+
+" id INTEGER PRIMARY KEY "+
+" username varchar(50) "+
+" password varchar(32) "+
+" epl_cod int, "+
+" tip_usr int, "+
+" estreg boolean "+
+"); "+
+
+" INSERT INTO usuario "+
+" (username,password,epl_cod,tip_usr, estreg) "+
+" VALUES ('andres','1f32aa4c9a1d2ea010adcf2348166a04',1,1,1); "+
+
+" INSERT INTO usuario "+
+" (username,password,epl_cod,tip_usr, estreg) "+
+" VALUES ('solinteg','47a7aa06274a88c7217954257329bb6e',1,1,1); ";
+
+
+INSERT INTO usuario (username,password,eplcod,tipusr, estreg) 
+VALUES ('andres','1f32aa4c9a1d2ea010adcf2348166a04',1,1,1); 
